4a28bc1ebf32f96518e67f6265fb06f2925f7f8e
Eric Camalionte <ecamalionte@gmail.com>
2018-06-26 00:51:20 -0300

    Configure API Routes with authentication context

--- web/router.ex
+++ web/router.ex
@@ -9,32 +9,43 @@ defmodule DoctorsApi.Router do
     plug :put_secure_browser_headers
   end
 
+  scope "/", DoctorsApi do
+    pipe_through :browser # Use the default browser stack
+    get "/", PageController, :index
+  end
+
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
-  pipeline :authenticated do
+  # this is a "maybe" authenticated pipeline
+  pipeline :auth do
     plug DoctorsApi.Guardian.AuthPipeline
   end
 
-  scope "/", DoctorsApi do
-    pipe_through :browser # Use the default browser stack
-
-    get "/", PageController, :index
+  # Use ensure auth to fail if there is no one logged in
+  pipeline :ensure_authenticated do
+    plug Guardian.Plug.EnsureAuthenticated
   end
 
-  # Other scopes may use custom stacks.
   scope "/api", DoctorsApi do
     pipe_through :api
+  end
+
+  # Maybe logged in routes
+  scope "/api", DoctorsApi do
+    pipe_through [:api, :auth]
 
     get "/", WelcomeApiController, :index
     post "/register", UserController, :create
     post "/login", SessionsController, :create
   end
 
+  # Definitely logged in routes
   scope "/api", DoctorsApi do
-    pipe_through [:api, :authenticated]
-    resources "/users", UserController, only: [:index, :show]
+    pipe_through [:api, :auth, :ensure_authenticated]
+    resources "/users", UserController
     delete "/logout", SessionsController, :delete
   end
 end
