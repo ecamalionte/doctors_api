b6b1065bb8eec0865a7abf5ab901e4dd5280eb14
Eric Camalionte <ecamalionte@gmail.com>
2018-06-30 23:33:14 -0300

    Add a test to the join channel scenario

--- test/channels/room_channel_test.exs
+++ test/channels/room_channel_test.exs
@@ -1,13 +1,19 @@
 defmodule DoctorsApi.RoomChannelTest do
   use Phoenix.ChannelTest
-  use DoctorsApi.ConnCase
+  use DoctorsApi.ChannelCase
+  import DoctorsApi.Factory
 
-  alias DoctorsApi.RoomChannel
+  alias DoctorsApi.{Guardian, UserSocket}
 
-  test "joining" do
-#     {:ok, _, socket} =
-#       socket("user:id", %{some_assigns: 1})
-#       |> subscribe_and_join(RoomChannel, "room:lobby", %{"id" => 3})
-#     IO.inspect(socket)
+  test "joining a channel" do
+    channel = insert(:user_channel)
+    user = List.first(channel.users)
+    {:ok, token, _} = Guardian.encode_and_sign(user)
+
+     {:ok, socket} = connect(UserSocket, %{"token" => token})
+     {:ok, _, socket} = subscribe_and_join(socket, "room:#{channel.id}", %{"token" => token})
+     assert socket.joined == true
+     assert socket.assigns.guardian_default_claims["sub"] == Integer.to_string(user.id)
+     assert socket.assigns.guardian_default_resource.name == user.name
   end
 end
